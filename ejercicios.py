59468d65e76bde8b6a94d0c76258a490aafbb948
Guerreroalejandro2110 <136136336+Guerreroalejandro2110@users.noreply.github.com>
2023-06-10 00:01:55 -0400

    Add files via upload

---
+++ ejercicios.py
@@ -0,0 +1,9 @@
+# Ejercicio Guerrero Alejandro
+
+
+print("'hola mundo'")  # comilla simple
+print('"hola mundo"')  # doble comilla
+print('hola "mundo"')  # comilla simple
+print("hola 'mundo'")  # comilla doble
+print("\\'hola mundo'\\")  # slash invertida con comilla simple
+print('\\"hola mundo"\\')  # slash invertida con comillas dobles
---
+++ ejercicios2.py
@@ -0,0 +1,24 @@
+# Ejercicio Guerrero Alejandro
+
+import math
+
+print("El resultado de suma es", 2 + 2)  # suma
+print("El resultado de resta es", 2 - 2)  # resta
+print("El resultado de multiplicacion es", 2 * 2)  # multiplicacion
+print("El resultado de division es", 2 / 2)  # division
+print("El resultado de division entera es", 2 // 2)  # division entera
+print("El resultado de modulo es", 2 & 2)  # modulo
+print("El resultado de potencia es", 2**2)  # potencia
+print("El resultado de operacion mixta es", 2 + 2 * 4)  # operacion mixta
+print("El resultado de operacion mixta es", (2 / 2 * 4) + 5)  # operacion mixta
+print("el resultado de logaritmo es", math.log(4))  # logaritmo
+print("el resultado de logaritmo es", math.log10(4))  # logaritmo de base 10
+print("el resultado de redondear es", round(4.9595959))  # Redondear numero
+print("el resultado del valor absoluto es", abs(-4))  # valor absoluto
+print("el numero minimo es", min(4, 6, 16, 2.5, 2.4))  # numero minimo
+print("el numero maximo es", max(11, 15, 16.6, 16.7))  # numero maximo
+print("el coseno es", math.cos(16))  # coseno
+print("el coseno es", math.sin(10))  # seno
+
+
+print("Fin de las operaciones")
